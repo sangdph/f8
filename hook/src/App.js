da3a2ee117f46d1029653392faa456bbc61dc267
sangdph <sangdph.developer@gmail.com>
2022-04-01 11:03:59 +0700

    two-way-binding

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,26 +1,10 @@
 import {useState} from 'react'
 import './App.css';
 
-const order = [100,200,300];
+const gifts = ['Xuân Nhi hôn Sang', 'Sang hôn Xuân Nhi', 'Xuân Nhi ôm Sang', 'Sang ôm Xuân Nhi ngủ'];
+console.log(gifts[1]);
 function App() {
-  const [Counter, setCounter] = useState(()=>{
-    const total = order.reduce((sang,nhi)=>nhi+sang)
-    console.log('sang');
-    return total;
-  });
-
-  const [Info, setInfo] = useState({
-    sang: "Đặng Phan Hoàng Sang",
-    nhi: "Nguyễn Ngọc Xuân Nhi"
-  })
-
-  const handleInfo = () =>{
-    setInfo({...Info,iu: "yêu"});
-  }
-
-  const handleCounter = () =>{
-    setCounter(prevState => prevState + 1);
-  }
+  
   return (
     <div className="App">
       <h1>{Info.nhi} {Info.iu} {Info.sang}</h1>
