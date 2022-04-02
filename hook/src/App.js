a2956910081f0bfe1c7ce13a99cf4e1bca4977c7
sangdph <sangdph.developer@gmail.com>
2022-04-02 19:58:46 +0700

    two-way-binding

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,15 +1,50 @@
 import {useState} from 'react'
 import './App.css';
 
-const gifts = ['Xuân Nhi hôn Sang', 'Sang hôn Xuân Nhi', 'Xuân Nhi ôm Sang', 'Sang ôm Xuân Nhi ngủ'];
-console.log(gifts[1]);
+const datas= [
+  {
+    id: 1,
+    name: 'Xuân Nhi'
+  },
+  {
+    id: 2,
+    name: 'Bé Sang'
+  },
+  {
+    id: 3,
+    name: 'Chị Xuân Nhi yêu Bé Sang'
+  }
+];
 function App() {
-  
+  const [checked, setChecked] = useState([]);
+  console.log(checked)
+  const handleSubmit = () =>{
+    console.log({ids: checked});
+  }
+  const handleCheck = (id) =>{
+    setChecked(prevState =>{
+      const isChecked = checked.includes(id);
+      if(isChecked){
+        return checked.filter(item => item !== id)
+      }else{
+        return [...prevState, id];
+      }
+    });
+  }
   return (
-    <div className="App">
-      <h1>{Info.nhi} {Info.iu} {Info.sang}</h1>
-      {console.log(Info)}
-      <button onClick={handleInfo}> Increase</button>
+    <div style={{padding: '32px'}}>
+      {datas.map(data=>(
+        <div key={data.id}>
+          <input 
+            type='checkbox' 
+            checked={checked.includes(data.id)}
+            onChange={()=>handleCheck(data.id)}
+          />
+          {data.name}
+        </div>
+      ))}
+      
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
