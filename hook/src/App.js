de600cbd2540653dba33d128a59004c0384887d4
sangdph <sangdph.developer@gmail.com>
2022-04-16 11:47:16 +0700

    useEffect

--- hook/src/App.js
+++ hook/src/App.js
@@ -4,35 +4,10 @@ import './App.css';
 
 
 function App() {
-  const [name, setName] = useState('')
-  const [items, setItems] = useState([]);
-
-  const Add = (name)=>{
-    setItems(prevState=>[...prevState, {name: name, done: true}]);
-    setName('');
-  }
-
-  const Delete = (value) =>{
-    setItems(items.filter(item => item.name!==value))
-  }
-
   const [show, setShow] = useState(false);
+
   return (
     <div style={{padding: '32px'}}>
-  
-    <input 
-      value={name}
-      onChange={e=>setName(e.target.value)}
-    />
-    <button onClick={()=>Add(name)}>Add</button>
-    <div>
-      {items.map((item,index)=>{
-        return  <div key={index} style={{marginTop: '5px'}}>
-                {item.name}<button onClick={()=>Delete(item.name)}>xóa</button>
-                <button >Hoàn thành 2</button>
-                </div>
-      })}
-    </div>
     <button onClick={()=>setShow(!show)}>Toggle</button>
     {show&&<Content/>}
     </div>
--- hook/src/content.js
+++ hook/src/content.js
@@ -1,7 +1,43 @@
+import {useState, useEffect} from 'react'
+const tasks=['posts', 'comments', 'albums'];
 function Content(){
+    const [title, setTitle] = useState('Chị Nhi yêu Sang');
+    const [posts, setPosts] = useState([]);
+    const [type, setType] = useState('posts');
+    useEffect(()=>{
+        document.title = title
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(res=>res.json())
+        .then(posts=>{
+            setPosts(posts);
+        })
+    },[type])
+
     return(
         <div>
+            {tasks.map(task=>(
+                <span key={task}>
+                    <button
+                        onClick={()=>setType(task)}
+                        style={type===task ? {
+                            color: '#fff',
+                            backgroundColor: '#333'
+                        }:{}}
+                    >
+                        {task}
+                    </button>
+                </span>
+            ))}
+            <input 
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+            />
             Xin chào chị Xuân Nhi
+            <ul>
+                {posts.map(post =>(
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
