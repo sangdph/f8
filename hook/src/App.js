4c6c7cce454f29bd6f04990fc005e6b0cb836e05
sangdph <sangdph.developer@gmail.com>
2022-06-04 14:28:13 +0700

    useRef-hook

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,15 +1,43 @@
-import {useState} from 'react'
-import Content from './content';
+import {useEffect, useRef, useState} from 'react'
 import './App.css';
 
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(60);
+
+  const timerId = useRef();
+  const sangPreCount = useRef();
+  const h1ref = useRef();
+
+  
+  useEffect(()=>{
+    const rest = h1ref.current.getBoundingClientRect();
+
+    console.log(rest);
+  })
+  useEffect(()=>{
+    sangPreCount.current = count;
+  }, [count])
+
+  const handleStart = ()=>{
+    timerId.current= setInterval(()=>{
+      setCount(prevCount =>prevCount -1);
+    }, 1000);
+    console.log("start: ", timerId.current);
+  }
+
+  const handleStop = ()=>{
+    clearInterval(timerId.current);
+    console.log("stop: ", timerId.current);
+  }
+
+  console.log(count, sangPreCount.current);
 
   return (
-    <div style={{padding: '32px'}}>
-    <button onClick={()=>setShow(!show)}>Toggle</button>
-    {show&&<Content/>}
+    <div style={{padding: '60px'}}>
+      <h1 ref={h1ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
